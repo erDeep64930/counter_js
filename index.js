8217b8c59e594f3c1ba59b2ee3bb195b4b96432f
Deepak Kumar <120357887+erDeep64930@users.noreply.github.com>
2023-06-04 20:18:59 +0530

    this is counter using js
    this is js

---
+++ index.js
@@ -0,0 +1,18 @@
+const countValue = document.querySelector('#counter');
+
+const increment = () => {
+    // get the value from ui
+    let value = parseInt(countValue.innerText);
+    // update the value 
+    value = value + 1;
+    // set the value
+    countValue.innerText = value;
+}
+
+const decrement = () => {
+    let value = parseInt(countValue.innerText);
+    // update the value 
+    value = value - 1;
+    // set the value
+    countValue.innerText = value;
+} 
